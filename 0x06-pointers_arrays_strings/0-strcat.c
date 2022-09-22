97825485d83de1427b2f8761575504e8cfda4fae
Ajiboye <Ajinoyeoluwatobi@gmail.com>
2022-09-22 10:33:18 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,13 @@
  *
  * Return: A pointer to the resulting string dest
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int dlen = 0, i;
+	int delen = 0, i;
 
 	while (dest[dlen] != '\0')
 	{
-		dlen++
+		dlen++;
 	}
 
 	for (i = 0; src[i] != 0; i++)
